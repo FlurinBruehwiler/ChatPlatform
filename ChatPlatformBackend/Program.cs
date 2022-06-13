d5cb2cdd91eefe4822a8d85fb0ede74497cde592
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-06-13 20:05:14 +0200

    feat(project): Initial commit [13.06.2022]

---
+++ ChatPlatformBackend/Program.cs
@@ -0,0 +1,54 @@
+using ChatPlatformBackend;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    // Identity made Cookie authentication the default.
+    // However, we want JWT Bearer Auth to be the default.
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    // Configure the Authority to the expected value for
+    // the authentication provider. This ensures the token
+    // is appropriately validated.
+    options.Authority = "Authority URL"; // TODO: Update URL
+
+    // We have to hook the OnMessageReceived event in order to
+    // allow the JWT authentication handler to read the access
+    // token from the query string when a WebSocket or 
+    // Server-Sent Events request comes in.
+
+    // Sending the access token in the query string is required due to
+    // a limitation in Browser APIs. We restrict it to only calls to the
+    // SignalR hub in this code.
+    // See https://docs.microsoft.com/aspnet/core/signalr/security#access-token-logging
+    // for more information about security considerations when using
+    // the query string to transmit the access token.
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+
+            // If the request is for our hub...
+            var path = context.HttpContext.Request.Path;
+            if (!string.IsNullOrEmpty(accessToken) &&
+                (path.StartsWithSegments("/hubs/chat")))
+            {
+                // Read the token out of the query string
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapHub<ChatHub>("/chatHub");
+app.Run();
