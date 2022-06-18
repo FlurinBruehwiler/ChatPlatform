362211d047cba5300f06a35fa4a80be91cac9bbd
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-06-18 20:15:50 +0200

    refactor(ErrorHandler): Moved Error Hander into seperate class [18.06.2022]

---
+++ ChatPlatformBackend/EndpointDefinitions/ExceptionHandlerEndpointDefinition.cs
@@ -0,0 +1,27 @@
+﻿using ChatPlatformBackend.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace ChatPlatformBackend.EndpointDefinitions;
+
+public class ExceptionHandlerEndpointDefinition : IEndpointDefinition
+{
+    public void DefineEndpoints(WebApplication app)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                var exceptionHandlerPathFeature =
+                    context.Features.Get<IExceptionHandlerPathFeature>();
+        
+                if (exceptionHandlerPathFeature?.Error is not BadRequestException exception)
+                    return;
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        
+                await context.Response.WriteAsJsonAsync(exception.Error);
+            });
+        });
+    }
+}
--- ChatPlatformBackend/Program.cs
+++ ChatPlatformBackend/Program.cs
@@ -1,6 +1,4 @@
-using ChatPlatformBackend.Exceptions;
 using ChatPlatformBackend.Extensions;
-using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,29 +8,9 @@ builder.AddServices();
 
 var app = builder.Build();
 
-app.UseExceptionHandler(appError =>
-{
-    appError.Run(async context =>
-    {
-        var exceptionHandlerPathFeature =
-            context.Features.Get<IExceptionHandlerPathFeature>();
-        
-        if (exceptionHandlerPathFeature?.Error is not BadRequestException exception)
-            return;
-
-        context.Response.ContentType = "application/json";
-        context.Response.StatusCode = StatusCodes.Status400BadRequest;
-        
-        await context.Response.WriteAsJsonAsync(exception.Error);
-    });
-});
-
 app.UseEndpointDefinitions();
-
 
 app.UseAuthentication();
 app.UseAuthorization();
-
-
 
 app.Run();
