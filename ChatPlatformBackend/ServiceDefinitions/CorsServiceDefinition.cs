f13b4661cd5e31ae86bf49ab6b61b39fb00bc625
FlurinBruehwilerCMI <flurin.bruehwiler@cmiag.ch>
2023-03-17 11:23:17 +0100

    fix(cors): added cors header

--- ChatPlatformBackend/ServiceDefinitions/CorsServiceDefinition.cs
+++ ChatPlatformBackend/ServiceDefinitions/CorsServiceDefinition.cs
@@ -9,7 +9,7 @@ public class CorsServiceDefinition : IServiceDefinition
             options.AddDefaultPolicy(policyBuilder =>
             {
                 policyBuilder
-                    .WithOrigins("https://localhost:3000", "http://localhost:3000")
+                    .WithOrigins("https://localhost:3000", "http://localhost:3000", "https://chatplatform-production-5907.up.railway.app")
                     .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
             });
         });
--- chat-platform-frontend/src/axios.ts
+++ chat-platform-frontend/src/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "https://localhost:7087";
+export const baseUrl = "https://chatplatform-production-5907.up.railway.app";
 
 const instance = axios.create({
   baseURL: baseUrl,
