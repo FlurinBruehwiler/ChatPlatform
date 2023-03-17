f5c6dbe38561574fc01fc7f41596e996efbc93d5
FlurinBruehwilerCMI <flurin.bruehwiler@cmiag.ch>
2023-03-17 11:26:42 +0100

    feat(cors): added http origin

--- ChatPlatformBackend/ServiceDefinitions/CorsServiceDefinition.cs
+++ ChatPlatformBackend/ServiceDefinitions/CorsServiceDefinition.cs
@@ -9,7 +9,7 @@ public class CorsServiceDefinition : IServiceDefinition
             options.AddDefaultPolicy(policyBuilder =>
             {
                 policyBuilder
-                    .WithOrigins("https://localhost:3000", "http://localhost:3000", "https://chatplatform-production-5907.up.railway.app")
+                    .WithOrigins("https://localhost:3000", "http://localhost:3000", "https://chatplatform-production-5907.up.railway.app", "http://chatplatform-production-5907.up.railway.app")
                     .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
             });
         });
